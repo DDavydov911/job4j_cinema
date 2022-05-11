8df973b4b74a842794f6a44fa29e54ec184e6dbb
Denis Davydov <dvd9@yandex.ru>
2022-05-11 18:03:25 +0300

    2. Сервис - Кинотеатр [#504869 #294021] added insert

--- db/scripts/update_002.sql
+++ db/scripts/update_002.sql
@@ -8,3 +8,10 @@ INSERT INTO users(username, email, phone, password) VALUES('Sasha', '123@mail.co
 INSERT INTO users(username, email, phone, password) VALUES('Dasha', '223@mail.com', '22345678', '223');
 INSERT INTO users(username, email, phone, password) VALUES('Masha', '323@mail.com', '32345678', '323');
 INSERT INTO users(username, email, phone, password) VALUES('Gosha', '423@mail.com', '42345678', '423');
+
+INSERT INTO tickets(session_id, row, cell, user_id)
+VALUES
+       (1, 1, 1, 1),
+       (1, 1, 2, 2),
+       (1, 1, 3, 3),
+       (1, 1, 4, 4);
